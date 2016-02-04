47734870dbe3ccc762f2f899e7fdb82f29b6d95c
zonell <neo_90ua@inbox.ru>
2016-02-04 17:44:15 +0200

    create class constants

--- app/src/main/java/net/validcat/st/wb/model/Constants.java
+++ app/src/main/java/net/validcat/st/wb/model/Constants.java
@@ -1,7 +1,5 @@
 package net.validcat.st.wb.model;
 
-/**
- * Created by Alex on 04.02.2016.
- */
 public class Constants {
+    public static final String URI_FEEDBACK = "https://play.google.com/store/apps/details?id=net.validcat.st.wb&ah=WK1XKkri7QOyZGYdcBkW2iIUK6o&hl=ru";
 }
