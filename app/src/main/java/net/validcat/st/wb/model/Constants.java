2f5ac78812fdc308f98549e4ceece4cd53b04ecd
zonell <neo_90ua@inbox.ru>
2016-02-04 17:42:49 +0200

    added icon

---
+++ app/src/main/java/net/validcat/st/wb/model/Constants.java
@@ -0,0 +1,7 @@
+package net.validcat.st.wb.model;
+
+/**
+ * Created by Alex on 04.02.2016.
+ */
+public class Constants {
+}
---
+++ app/src/main/java/net/validcat/st/wb/support/SettingsActivity.java
@@ -0,0 +1,7 @@
+package net.validcat.st.wb.support;
+
+/**
+ * Created by Alex on 04.02.2016.
+ */
+public class SettingsActivity {
+}
