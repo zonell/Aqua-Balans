c9770f0ff31a2deb436222c7c3769f39cebdabd6
zonell <neo_90ua@inbox.ru>
2016-02-05 23:51:28 +0200

    Class constants creation

--- app/src/main/java/net/validcat/st/wb/model/Constants.java
+++ app/src/main/java/net/validcat/st/wb/model/Constants.java
@@ -1,5 +1,16 @@
 package net.validcat.st.wb.model;
 
 public class Constants {
-    public static final String URI_FEEDBACK = "https://play.google.com/store/apps/details?id=net.validcat.st.wb&ah=WK1XKkri7QOyZGYdcBkW2iIUK6o&hl=ru";
+    public static final String URI_FEEDBACK = "https://play.google.com/store/apps/details?id=" +
+            "net.validcat.st.wb&ah=WK1XKkri7QOyZGYdcBkW2iIUK6o&hl=ru";
+
+    public static final String COUNT_BOTTLE = "count_bottle";
+    public static final String COUNT_CANCEL = "count_cancel";
+    public static final String COUNT_DRINK = "count_drink";
+
+    public static int COUNT_MENU = 1;
+    public static final  int MENU_PROGRESS = 1;
+    public static final  int MENU_STASTIC = 2;
+    public static final  int MENU_NOTIFICATION = 3;
+    public static final  int MENU_NEWS = 4;
 }
