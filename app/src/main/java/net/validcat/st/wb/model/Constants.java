ac99a2480017d26b98678f1c1ee4750046652821
zonell <neo_90ua@inbox.ru>
2016-02-06 19:23:48 +0200

    rename constants

--- app/src/main/java/net/validcat/st/wb/model/Constants.java
+++ app/src/main/java/net/validcat/st/wb/model/Constants.java
@@ -4,13 +4,13 @@ public class Constants {
     public static final String URI_FEEDBACK = "https://play.google.com/store/apps/details?id=" +
             "net.validcat.st.wb&ah=WK1XKkri7QOyZGYdcBkW2iIUK6o&hl=ru";
 
-    public static final String COUNT_BOTTLE = "count_bottle";
     public static final String COUNT_CANCEL = "count_cancel";
     public static final String COUNT_DRINK = "count_drink";
+    public static final String DRINK_MIN = "drink_volume_min";
+    public static final String DRINK_MAX = "drink_volume_max";
 
     public static int COUNT_MENU = 1;
     public static final  int MENU_PROGRESS = 1;
     public static final  int MENU_STASTIC = 2;
     public static final  int MENU_NOTIFICATION = 3;
-    public static final  int MENU_NEWS = 4;
 }
--- app/src/main/java/net/validcat/st/wb/support/BottleParams.java
+++ app/src/main/java/net/validcat/st/wb/support/BottleParams.java
@@ -3,13 +3,15 @@ package net.validcat.st.wb.support;
 import net.validcat.st.wb.R;
 
 public class BottleParams {
-    public static final int COUNT_CANCEL = 1;
-    public static final int CANCEL_200_ML = 2;
-    public static final int CANCEL_400_ML = 3;
+    public static final int COUNT_CANCEL = -1;
+    public static final int CANCEL_MIN = 2;
+    public static final int CANCEL_MAX = 3;
 
     public static final int FULL = R.string.full;
 
+    public static int percent_drink = 0;
     public static int count_drink = 0;
+    public static int aqua_balance = 0;
     public static int count_cancel = COUNT_CANCEL;
 
 }
