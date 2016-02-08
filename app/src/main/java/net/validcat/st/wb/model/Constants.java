5255a50e64ef4836ea5771012fbdbdf39532f2d9
zonell <neo_90ua@inbox.ru>
2016-02-08 17:19:05 +0200

    replacing images and constants

--- app/src/main/java/net/validcat/st/wb/model/Constants.java
+++ app/src/main/java/net/validcat/st/wb/model/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final String URI_FEEDBACK = "https://play.google.com/store/apps/details?id=" +
             "net.validcat.st.wb&ah=WK1XKkri7QOyZGYdcBkW2iIUK6o&hl=ru";
 
-    public static final String COUNT_CANCEL = "count_cancel";
+    public static final String COUNT_CANCEL = "cancel";
     public static final String COUNT_DRINK = "count_drink";
     public static final String DRINK_MIN = "drink_volume_min";
     public static final String DRINK_MAX = "drink_volume_max";
--- app/src/main/java/net/validcat/st/wb/support/BottleParams.java
+++ app/src/main/java/net/validcat/st/wb/support/BottleParams.java
@@ -3,15 +3,11 @@ package net.validcat.st.wb.support;
 import net.validcat.st.wb.R;
 
 public class BottleParams {
-    public static final int COUNT_CANCEL = -1;
-    public static final int CANCEL_MIN = 2;
-    public static final int CANCEL_MAX = 3;
-
     public static final int FULL = R.string.full;
+    public static final int EMPTY = 0;
 
     public static int percent_drink = 0;
     public static int count_drink = 0;
     public static int aqua_balance = 0;
-    public static int count_cancel = COUNT_CANCEL;
 
 }
