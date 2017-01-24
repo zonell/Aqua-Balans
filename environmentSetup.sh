a0e4d50c19dbe736f11f0d5b69a97e48a1d593bc
Aleksander <aleksandr90dziuba@gmail.com>
2017-01-24 11:08:15 +0200

    Update environmentSetup.sh

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -1,20 +1,30 @@
-#!/bin/bash
+general:
+  artifacts:
+    -/home/ubuntu/**repo_name**/build/outputs/reports/**testFolderName**/connected
 
-# Fix the CircleCI path
-function getAndroidSDK(){
-  export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+machine:
+  environment:
+    ANDROID_HOME: /home/ubuntu/android
+  java:
+    version: oraclejdk6
 
-  DEPS="$ANDROID_HOME/installed-dependencies"
+dependencies:
+  cache_directories:
+    - ~/.android
+    - ~/android
+  override:
+    - (echo "Downloading Android SDK v19 now!")
+    - (source scripts/environmentSetup.sh && getAndroidSDK)
 
-
-function waitForAVD {
-    (
-    local bootanim=""
-    export PATH=$(dirname $(dirname $(which android)))/platform-tools:$PATH
-    until [[ "$bootanim" =~ "stopped" ]]; do
-      sleep 5
-      bootanim=$(adb -e shell getprop init.svc.bootanim 2>&1)
-      echo "emulator status=$bootanim"
-    done
-    )
-}
+test:
+  pre:
+    - $ANDROID_HOME/tools/emulator -avd testAVD -no-skin -no-audio -no-window:
+      background: true
+    - (./gradlew assembleDebug):
+      timeout: 1200
+    - (./gradlew assembleDebugTest):
+      timeout: 1200
+    - (source scripts/environmentSetup.sh && waitForAVD)
+  override:
+    - (echo "Running JUnit tests!")
+    - (./gradlew connectedAndroidTest)
