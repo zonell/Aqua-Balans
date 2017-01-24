89fbb1752ed65481e1ad275614b20ea84e98410a
Aleksander <aleksandr90dziuba@gmail.com>
2017-01-24 10:18:50 +0200

    Update environmentSetup.sh

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -6,17 +6,6 @@ function getAndroidSDK(){
 
   DEPS="$ANDROID_HOME/installed-dependencies"
 
-  if [ ! -e $DEPS ]; then
-    cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
-    echo y | android update sdk -u -a -t android-19 &&
-    echo y | android update sdk -u -a -t platform-tools &&
-    echo y | android update sdk -u -a -t build-tools-21.1.2 &&
-    echo y | android update sdk -u -a -t sys-img-x86-android-19 &&
-    #echo y | android update sdk -u -a -t addon-google_apis-google-18 &&
-    echo no | android create avd -n testAVD -f -t android-19 --abi default/x86 &&
-    touch $DEPS
-  fi
-}
 
 function waitForAVD {
     (
