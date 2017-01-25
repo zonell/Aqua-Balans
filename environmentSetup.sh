abb44849125df91a5fb4e3d562f78372bbc50947
Aleksander <aleksandr90dziuba@gmail.com>
2017-01-25 12:19:28 +0200

    Update environmentSetup.sh

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
 # Fix the CircleCI path
-function getAndroidSDK(){
-  export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 
-  DEPS="$ANDROID_HOME/installed-dependencies"
+DEPS="$ANDROID_HOME/installed-dependencies"
 
-  if [ ! -e $DEPS ]; then
-    cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
-    echo y | android update sdk -u -a -t android-19 &&
-    echo y | android update sdk -u -a -t platform-tools &&
-    echo y | android update sdk -u -a -t build-tools-21.1.2 &&
-    echo y | android update sdk -u -a -t sys-img-x86-android-19 &&
-    #echo y | android update sdk -u -a -t addon-google_apis-google-19 &&
-    echo no | android create avd -n testAVD -f -t android-19 --abi default/x86 &&
-    touch $DEPS
-  fi
-}
-
-function waitAVD {
-    (
-    local bootanim=""
-    export PATH=$(dirname $(dirname $(which android)))/platform-tools:$PATH
-    until [[ "$bootanim" =~ "stopped" ]]; do
-      sleep 5
-      bootanim=$(adb -e shell getprop init.svc.bootanim 2>&1)
-      echo "emulator status=$bootanim"
-    done
-    )
-}
+if [ ! -e $DEPS ]; then
+  cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
+  echo y | android update sdk -u -a -t build-tools-20.0.0 &&
+  echo y | android update sdk -u -a -t android-19 &&
+  echo y | android update sdk -u -a -t extra-android-m2repository &&
+  echo y | android update sdk -u -a -t extra-google-m2repository &&
+  touch $DEPS
+fi
